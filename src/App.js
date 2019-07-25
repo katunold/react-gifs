4a0ff920534dcd89c63149348547d32f853071a6
katunold <katunold94@gmail.com>
2019-07-25 12:12:07 +0300

    - initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
+import GifList from './Components/GifList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    gifs: [],
+    loading: true
+  };
+
+  componentDidMount() {
+    this.performSearch()
+  }
+
+  performSearch = (query = 'jokes') => {
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${query}&limit=24&api_key=dc6zaTOxFJmzC`)
+      .then(response => {
+        this.setState({ gifs: response.data.data, loading: false })
+      })
+      .catch(error => console.log(error));
+  };
+
+  render() {
+    console.log(this.state.gifs);
+    const { loading } = this.state;
+    return (
+      <div>
+        <div className="main-header">
+          <div className="inner">
+            <h1 className="main-title">GifSearch</h1>
+            <SearchForm onSearch={this.performSearch} />
+          </div>
+        </div>
+        <div className="main-content">
+          {
+            loading
+              ? <p>Loading...</p>
+              : <GifList data={this.state.gifs}/>
+          }
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
